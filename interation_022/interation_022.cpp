5f6004d4200e71bb564f7caf0625c7259e80d3db
elwinspranata <elwinpranata02@gmail.com>
2023-03-23 11:21:30 +0700

    membuat prosedur insertionsort

--- interation_022/interation_022.cpp
+++ interation_022/interation_022.cpp
@@ -28,8 +28,34 @@ void  input() {
     }
 
 }
+void insertionsort() {       // Procedure Insertionsort }
 
-int main() {
+    Int temp;                // Membuat variable data temporer atau penyimpanan sementara 
+    Int j;                   // Mmebuat variable j sebagai penanda 
+
+    for (int i = 1; i < n; i++) {       // Looping dengan i dimulai dari 1 hingga n-1 
+
+        temp = arr[i];                  // simpan nilai arr[i] ke variable sementara temp 
+
+        j = i - 1;                      // setting nilai j sama dengan i-1; 
+
+        while (j >= 0 && arr[j] > temp)  // Loopig while dimana nilai j lebih besar sama dengan 0 dan 
+            // arr[j] lebih besar dari pada temp 
+        {
+            arr[j + 1] = arr[j];         // Simpan arr[j] ke dalam variable arr[j+1] 
+            j--;                         // Decrement nilai j by 1 
+        }
+
+        arr[j + 1] = arr[j];
+
+        cout << "\nStep " << i << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+        }
+    }
+}
 
 
+         }
+    }
 }
