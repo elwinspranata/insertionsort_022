3baa8328e239b393bf276d607799f8dcf6697668
elwinspranata <elwinpranata02@gmail.com>
2023-03-23 10:55:16 +0700

    Add project files.

---
+++ interation_022/interation_022.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];
+int n;
+
+void  input() {
+
+    while (true) {
+        cout << "masukan elemen/jumlah data array : \n";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "array hanya bisa memiliki maksimal 20 elemen";
+        }
+    }
+
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "     masukan elemen array        " << endl;
+    cout << "=================================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "masukan data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
+
+}
+
+int main() {
+
+}
